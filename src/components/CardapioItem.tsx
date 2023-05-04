5ade28a8c31dda77c44df2c2848429fbc419e8b8
danibex <danielivam96@gmail.com>
2023-05-04 20:05:52 -0300

    .

--- src/components/CardapioItem.tsx
+++ src/components/CardapioItem.tsx
@@ -1,15 +1,30 @@
 import { IconShoppingCart } from "@tabler/icons-react"
 import Image from "next/image"
 import blackBurguer from "@/images/black-burguer.png"
+import { useState } from "react"
 interface CardapioItemProps {
     id: number
     nome: string
     descricao: string
-    preco: any
+    preco: number
     url: any
 }
 export default function CardapioItem(props: CardapioItemProps): JSX.Element {
-    return(
+    let [quantidade, setQuantidade] = useState(0)
+    let [valor, setValor] = useState(props.preco)
+    function acrescentar() {
+        quantidade >=0 ? setQuantidade(++quantidade) : ""
+        atualizarPreco()
+    }
+    function diminuir() {
+        quantidade >=1 ? setQuantidade(--quantidade) : ""
+        
+        atualizarPreco()
+    }
+    function atualizarPreco() {
+        quantidade >=1 ? setValor(valor = props.preco * quantidade) : ""
+    }
+     return(
     <div className="flex flex-col my-8 mx-1/2 shadow-xl p-3 rounded-md">
         <div className="m-3">
             <h2 className="text-lg text-red-600 font-semibold m-1">{props.nome}</h2>
@@ -19,12 +34,12 @@ export default function CardapioItem(props: CardapioItemProps): JSX.Element {
             <Image src={blackBurguer} alt={props.descricao} width={200} height={200}/>
         </div>
         <div className="mx-2 my-0 flex flex-row items-center justify-start">
-            <p className="font-black mr-10 ml-0 text-xl">{props.preco}</p>
+            <p className="font-black mr-10 ml-0 text-xl">R$ {valor},00</p>
             <div className="flex flex-row m-2">
                 <span className="mx-1 bg-gray-300 rounded-md flex flex-row">
-                    <button className="p-2">-</button>
-                    <p className="p-2">0</p>
-                    <button className="p-2">+</button>
+                    <button className="p-2" onClick={diminuir}>-</button>
+                    <p className="p-2" >{quantidade}</p>
+                    <button className="p-2" onClick={acrescentar}>+</button>
                 </span>
                 <button className="bg-red-700 rounded-md p-1 mx-1"><IconShoppingCart className="text-white font-black"/></button>
             </div>
--- src/data/Produtos.ts
+++ src/data/Produtos.ts
@@ -4,42 +4,42 @@ export const produtos = [
         id: 1,
         nome: "Hamburguer",
         descricao: "Black Angus Burguer",
-        preco: "R$ 15,00",
+        preco: 15,
         url: {blackBurguer}
     },
     {
         id: 2,
         nome: "Hamburguer",
         descricao: "Black Angus Burguer",
-        preco: "R$ 15,00",
+        preco: 15,
         url: {blackBurguer}
     },
     {
         id: 3,
         nome: "Hamburguer",
         descricao: "Black Angus Burguer",
-        preco: "R$ 15,00",
+        preco: 15,
         url: {blackBurguer}
     },
     {
         id: 4,
         nome: "Hamburguer",
         descricao: "Black Angus Burguer",
-        preco: "R$ 15,00",
+        preco: 15,
         url: {blackBurguer}
     },
     {
         id: 5,
         nome: "Hamburguer",
         descricao: "Black Angus Burguer",
-        preco: "R$ 15,00",
+        preco: 15,
         url: {blackBurguer}
     },
     {
         id: 6,
         nome: "Hamburguer",
         descricao: "Black Angus Burguer",
-        preco: "R$ 15,00",
+        preco: 15,
         url: {blackBurguer}
     },
 ]
