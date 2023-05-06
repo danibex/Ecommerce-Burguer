dc8143222860b04aa94d7ae440f0974e1e4b7b40
danibex <danielivam96@gmail.com>
2023-05-05 23:09:49 -0300

    .

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,11 @@
+import { IconAddressBook, IconHome } from "@tabler/icons-react"
 import MenuItem from "./MenuItem"
 
 export default function Menu() {
     return(
     <> 
-        <MenuItem url="/home">Home</MenuItem>
-        <MenuItem url="/contato">Contato</MenuItem>
+        <MenuItem url="/home"><IconHome/>Home</MenuItem>
+        <MenuItem url="/contato"><IconAddressBook/>Contato</MenuItem>
     </>    
     )
 }
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 interface MenuItemProps {
-    children: string
+    children: any
     url: string
 }
 export default function MenuItem(props: MenuItemProps) {
     return(
-        <li className="m-4 text-xl hover:text-gray-500"><Link href={props.url}>{props.children}</Link></li>
+        <li className="m-4 text-xl hover:text-gray-500 flex flex-row flex-nowrap"><Link className="flex flex-row flex-nowrap" href={props.url}>{props.children}</Link></li>
     )
 }
