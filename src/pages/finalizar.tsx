4f1e5232379143d5b226df9913096e8884109d07
danibex <danielivam96@gmail.com>
2023-05-07 20:06:38 -0300

    .

---
+++ src/pages/finalizar.tsx
@@ -0,0 +1,25 @@
+import Navegacao from "@/components/Navegacao";
+
+export default function Finalizar() {
+    return (
+<div className="h-[100vh] p-1 m-0">
+    <Navegacao>
+        <div className="flex flex-row w-[100%] h-full p-5 justify-center items-center">
+            <div className="w-[55%] flex flex-col justify-center items-center">
+                <div className="flex flex-col justify-start items-start">
+                    <div>icone<h1>Endereço de Entrega</h1></div>
+                    <p>Informe o endereço onde deseja receber o seu pedido.</p>   
+                </div>
+                
+                
+                
+                <div>Pagamento</div>
+            </div>
+            <div className="w-[45%]">
+                Resumo do pedido
+            </div>
+        </div>
+    </Navegacao>
+</div>
+    )
+}
