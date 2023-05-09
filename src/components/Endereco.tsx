3fad9d50e19a8614364280a0051f08d3860cf7f5
danibex <danielivam96@gmail.com>
2023-05-09 20:16:32 -0300

    adicionando formas de pagamento

---
+++ src/components/Endereco.tsx
@@ -0,0 +1,51 @@
+import { IconMapPin, IconSearch } from "@tabler/icons-react";
+import { useState } from "react";
+
+export default function Finalizar() {
+    const [cep, setCep] = useState("");
+    const [cepRaw, setCepRaw] = useState("");
+    const [enderecoCompleto, setEnderecoCompleto] = useState({logradouro: "", bairro: "", localidade: "", uf: ""})
+    function FormatarCep(event: { target: { value: any; }; }) {
+    let cepValue = event.target.value;
+
+    // Remove tudo que não é número
+    cepValue = cepValue.replace(/\D/g, "");
+
+    // Formata o CEP com hífen
+    const formattedCep = cepValue.replace(/^(\d{5})(\d{3})?$/, "$1-$2");
+
+    // Atualiza o estado com o CEP formatado e não formatado
+    setCep(formattedCep);
+    setCepRaw(cepValue);
+    console.log(cepRaw)
+}
+
+function preencherEndereco() {
+const url = `https://viacep.com.br/ws/${cepRaw}/json/` 
+    fetch(url)
+        .then(resp =>  resp.json()) 
+        .then(endereco => { setEnderecoCompleto(endereco); console.log(enderecoCompleto)})
+        .catch(error => {
+            window.alert(`[ERRO] ${error}`)
+          })
+}
+
+    return (
+        <div className="flex flex-col justify-start items-start shadow-lg p-3 rounded-lg">
+            <div className="flex flex-row"><IconMapPin className="text-red-600"/><h1>Endereço de Entrega</h1></div>
+            <p>Informe o endereço onde deseja receber o seu pedido:</p>  
+            <form onSubmit={(e) => { e.preventDefault(); preencherEndereco() }}>
+                <div className="w-[100%] flex justify-start items-center ">
+                <input onChange={FormatarCep} value={cep} className="bg-gray-200 rounded-md m-2 p-2 w-[40%]" type="text" name="CEP" id="CEP" placeholder="CEP"/>
+                <button className="bg-red-600 text-white rounded-xl p-2 flex justify-center items-center"><IconSearch className="font-black"/></button>
+                </div>
+                <input className="bg-gray-200 rounded-md m-2 p-2 w-[95%]" type="text" name="Rua" id="Rua" placeholder="Rua" readOnly value={enderecoCompleto.logradouro}/>
+                <input className="bg-gray-200 rounded-md m-2 p-2 w-[46%]" type="text" name="Numero" id="Numero" placeholder="Número" />
+                <input className="bg-gray-200 rounded-md m-2 p-2 w-[46%]" type="text" name="Complemento" id="Complemento" placeholder="Complemento" />
+                <input className="bg-gray-200 rounded-md m-2 p-2 w-[38%]" type="text" name="Bairro" id="Bairro" placeholder="Bairro" readOnly value={enderecoCompleto.bairro}/>
+                <input className="bg-gray-200 rounded-md m-2 p-2 w-[38%]" type="text" name="Cidade" id="Cidade" placeholder="Cidade" readOnly value={enderecoCompleto.localidade}/>
+                <input className="bg-gray-200 rounded-md m-2 p-2 w-[10%]" type="text" name="UF" id="UF" placeholder="UF" readOnly value={enderecoCompleto.uf}/>
+            </form> 
+        </div> 
+    )
+}
--- src/pages/finalizar.tsx
+++ src/pages/finalizar.tsx
@@ -1,40 +1,36 @@
+import Endereco from "@/components/Endereco";
 import Navegacao from "@/components/Navegacao";
-import { IconMapPin, IconSearch } from "@tabler/icons-react";
-
+import {  IconCashBanknote, IconCreditCard, IconQrcode, IconWallet } from "@tabler/icons-react";
+import Image from "next/image";
+import Visa from "@/images/Bandeiras/Visa.png"
+import Master from "@/images/Bandeiras/Master.png"
+import Cielo from "@/images/Bandeiras/Cielo.png"
 export default function Finalizar() {
-    function subir() {
-        console.log("Enviado!!!")
-    }
     return (
 <div className="h-[100vh] p-1 m-0">
     <Navegacao>
         <div className="flex flex-col sm:flex-row w-[100%] h-full p-5 justify-center items-center">
             <div className="w-[100%] sm:w-[55%] flex flex-col justify-center items-center">
-                <div className="flex flex-col justify-start items-start shadow-xl p-3 rounded-lg">
-                    <div className="flex flex-row"><IconMapPin className="text-red-600"/><h1>Endereço de Entrega</h1></div>
-                    <p>Informe o endereço onde deseja receber o seu pedido:</p>  
-                    <form onSubmit={subir}>
-                        <div className="w-[100%] flex justify-start items-center ">
-                        <input className="bg-gray-200 rounded-md m-2 p-2 w-[40%]" type="text" name="CEP" id="CEP" placeholder="CEP"/>
-                        <button className="bg-red-600 text-white rounded-xl p-2 flex justify-center items-center"><IconSearch className="font-black"/></button>
-                        </div>
-                        <input className="bg-gray-200 rounded-md m-2 p-2 w-[95%]" type="text" name="Rua" id="Rua" placeholder="Rua" />
-                        <input className="bg-gray-200 rounded-md m-2 p-2 w-[46%]" type="text" name="Numero" id="Numero" placeholder="Número"/>
-                        <input className="bg-gray-200 rounded-md m-2 p-2 w-[46%]" type="text" name="Complemento" id="Complemento" placeholder="Complemento"/>
-                        <input className="bg-gray-200 rounded-md m-2 p-2 w-[38%]" type="text" name="Bairro" id="Bairro" placeholder="Bairro"/>
-                        <input className="bg-gray-200 rounded-md m-2 p-2 w-[38%]" type="text" name="Cidade" id="Cidade" placeholder="Cidade"/>
-                        <input className="bg-gray-200 rounded-md m-2 p-2 w-[10%]" type="text" name="UF" id="UF" placeholder="UF"/>
-                    </form> 
+                <Endereco/>
+                <div className="flex flex-col justify-start items-start shadow-xl p-5 rounded-lg w-[100%] my-3">
+                    <div className="flex flex-row justify-center items-center"><IconWallet size={25} className="text-red-600"/><h1 className="text-2xl font-black">Pagamento</h1></div>
+                    <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+                    <h2 className="text-lg font-bold">Aceitamos:</h2>
+                    <div className="flex flex-row w-[100%] justify-between p-2 items-center my-2">
+                        <Image src={Visa} alt="Bandeira Visa"/>
+                        <Image src={Master} alt="Bandeira Master"/>
+                        <Image src={Cielo} alt="Bandeira Cielo"/>
+                    </div>
+                    <div className="flex flex-row justify-between items-center w-[100%] my-2">
+                        <button className="flex fled-row hover:bg-gray-400 active:bg-gray-500 justify-around bg-gray-300 text-black rounded-md p-3 font-bold"><IconCreditCard className="mr-2 text-red-500"/>Débito</button>
+                        <button className="flex fled-row hover:bg-gray-400 active:bg-gray-500 justify-around bg-gray-300 text-black rounded-md p-3 font-bold"><IconCreditCard className="mr-2 text-red-500"/>Credito</button>
+                        <button className="flex fled-row hover:bg-gray-400 active:bg-gray-500 justify-around bg-gray-300 text-black rounded-md p-3 font-bold"><IconCashBanknote className="mr-2 text-red-500"/>Dinheiro</button>
+                        <button className="flex fled-row hover:bg-gray-400 active:bg-gray-500 justify-around bg-gray-300 text-black rounded-md p-3 font-bold"><IconQrcode className="mr-2 text-red-500"/>Pix</button>
+                    </div>
                 </div>
-                
-                
-                
-                <div>
-                    Pagamento
-                </div>
-
-
             </div>
+
+
             <div className="w-[100%] sm:w-[45%]">
                 Resumo do pedido
             </div>
